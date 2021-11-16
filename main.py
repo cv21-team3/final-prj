2146de851e5c14643f7bb27cda9fd520f25e5b1b
gajagajago <gajagajago@naver.com>
2021-11-16 20:39:14 +0900

    Add samples

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
-def select_removal_target():
-    return
+def select_removal_target(igs_in):
+    for x in range(igs_in.shape[0]):
+        if x not in range(140, 322):
+            igs_in[x, :, :] = 0
+        else:
+            for y in range(igs_in.shape[1]):
+                if y not in range(210, 270):
+                    igs_in[x, y, :] = 0
+
+    if igs_in is not None:
+        plt.figure()
+        plt.imshow(igs_in.astype(np.uint8))
+        plt.axis()
+        plt.show()
+
+    return igs_in
 
 def mask(igs_in):
     for x in range(180, 278):
@@ -34,19 +48,20 @@ def main():
     ##############
     # step 1: selecting removal target region
     ##############
-    #select_removal_target()
+    target = select_removal_target(igs_in.copy())
+    Image.fromarray(target.astype(np.uint8)).save('data/target/(예시)1-2.png')
 
     ##############
     # step 2: masking removal target region
     ##############
-    masked = mask(igs_in)
-    Image.fromarray(masked.astype(np.uint8)).save('data/masked/masked_1.png')
+    masked = mask(igs_in.copy())
+    Image.fromarray(masked.astype(np.uint8)).save('data/masked/(예시)1-2.png')
 
     ##############
     # step 3: repainting masked region
     ##############
     repainted = repaint(masked)
-    Image.fromarray(repainted.astype(np.uint8)).save('data/result/result_1.png')
+    Image.fromarray(repainted.astype(np.uint8)).save('data/result/(예시)1-2.png')
 
 if __name__ == '__main__':
     main()
